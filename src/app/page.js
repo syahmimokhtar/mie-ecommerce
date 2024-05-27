0eafd45bdc6c240b01d7643def98af9a280b1880
syahmi mokhtar <syahmiez96@gmail.com>
2024-05-27 19:09:36 +0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+export default function Home() {
+  return (
+    <>
+       <Head>
+        <link  rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+
+      <main>
+        asas
+      </main>
+
+      <Footer />
+  </> 
+  );
+}
